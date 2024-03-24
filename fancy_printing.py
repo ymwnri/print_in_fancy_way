bf0dbeb3d55cbe554d4694987e28aa642958b0bd
Aidan Tiu <tiuprof77@gmail.com>
2024-03-24 11:42:03 +0800

    Add initializing bar using print

--- fancy_printing.py
+++ fancy_printing.py
@@ -6,3 +6,17 @@
 first_name = input("Enter your First Name: ")
 last_name = input("Enter your Last Name: ")
 dream_job = input("Enter your Dream Job: ") 
+
+# Print initializing bar
+print("\nInitializing . . . . .")
+
+def progress(percent=0, width=30):
+    left = width * percent // 100
+    right = width - left
+    print('\r[', '#' * left, ' ' * right, ']',
+          f' {percent:.0f}%',
+          sep='', end='', flush=True)
+
+for i in range(101):
+    progress(i)
+    sleep(0.1)
