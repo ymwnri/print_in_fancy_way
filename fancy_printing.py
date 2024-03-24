d5f0281740b0a62a78a57f22dd638b1ba07ea081
Aidan Tiu <tiuprof77@gmail.com>
2024-03-24 11:40:00 +0800

    Add questions for user input

--- fancy_printing.py
+++ fancy_printing.py
@@ -1,3 +1,8 @@
 # Ask for name, last name and dream job
 # Display the user info in a fancy way
 # Print a progress bar while the program is loading
+
+# Ask for user input
+first_name = input("Enter your First Name: ")
+last_name = input("Enter your Last Name: ")
+dream_job = input("Enter your Dream Job: ") 
